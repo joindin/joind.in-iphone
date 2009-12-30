532242e5de13d2a9a0a7afe23362d17087eeeb73
Kevin <kevin@edoras.local>
2009-12-30 18:08:56 +0000

    First calling of API, thought it was worth a commit

---
+++ joindinapp/Classes/joindinappAppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  joindinappAppDelegate.m
+//  joindinapp
+//
+//  Created by Kevin on 29/12/2009.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "joindinappAppDelegate.h"
+
+
+@implementation joindinappAppDelegate
+
+@synthesize window;
+@synthesize tabBarController;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    
+    // Add the tab bar controller's current view as a subview of the window
+    [window addSubview:tabBarController.view];
+}
+
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+}
+*/
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+}
+*/
+
+
+- (void)dealloc {
+    [tabBarController release];
+    [window release];
+    [super dealloc];
+}
+
+@end
+
