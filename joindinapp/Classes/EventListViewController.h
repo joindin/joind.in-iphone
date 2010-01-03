212906e09df0ee060b7a537e3a0ae164aa57eb23
Kevin Bowman <kevin@magicmonkey.org>
2010-01-03 00:26:21 +0000

    Added a splash screen

---
+++ joindinapp/Classes/EventListViewController.h
@@ -0,0 +1,17 @@
+//
+//  EventListViewController.h
+//  joindinapp
+//
+//  Created by Kevin on 31/12/2009.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "EventListModel.h"
+
+@interface EventListViewController : UITableViewController {
+	EventListModel *confListData;
+}
+
+@property(nonatomic, retain) EventListModel *confListData;
+
+@end
--- joindinapp/Classes/joindinappAppDelegate.h
+++ joindinapp/Classes/joindinappAppDelegate.h
@@ -6,14 +6,16 @@
 //  Copyright __MyCompanyName__ 2009. All rights reserved.
 //
 
+#import "SplashScreenViewController.h"
+
 @interface joindinappAppDelegate : NSObject <UIApplicationDelegate> {
     
     UIWindow *window;
-    UINavigationController *navigationController;
+    IBOutlet SplashScreenViewController *splashScreenViewController;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, retain) IBOutlet SplashScreenViewController *splashScreenViewController;
 
 @end
 
