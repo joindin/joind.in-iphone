403ae7736279bff4a1905db53592c9749738990a
Kevin Bowman <kevin@magicmonkey.org>
2010-01-04 21:52:33 +0000

    Better activity indicator in the event list screen

--- joindinapp/Classes/TalkDetailViewController.m
+++ joindinapp/Classes/TalkDetailViewController.m
@@ -45,34 +45,6 @@
     [super didReceiveMemoryWarning];
 }
 
-#pragma mark Table view methods
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-
-// Customize the number of rows in the table view.
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
-}
-
-
-// Customize the appearance of table view cells.
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-    // Set up the cell...
-	
-    return cell;
-}
-
 - (void)dealloc {
     [super dealloc];
 }
