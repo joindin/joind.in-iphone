3f5e49941c0cd1747b07e7313ee31df77f87b9d1
Kevin Bowman <kevin@magicmonkey.org>
2010-01-15 21:27:21 +0000

    Added ability to rate a talk when commenting

--- joindinapp/Classes/NewCommentViewCell.h
+++ joindinapp/Classes/NewCommentViewCell.h
@@ -13,6 +13,13 @@
 	IBOutlet UIButton *uiSubmit;
 	IBOutlet UITextView *uiComment;
 	IBOutlet UIActivityIndicatorView *uiActivity;
+	IBOutlet UIButton *uiRating1;
+	IBOutlet UIButton *uiRating2;
+	IBOutlet UIButton *uiRating3;
+	IBOutlet UIButton *uiRating4;
+	IBOutlet UIButton *uiRating5;
+	
+	NSUInteger rating;
 	
 	id commentDelegate;
 }
@@ -21,11 +28,18 @@
 @property (nonatomic, retain) UITextView *uiComment;
 @property (nonatomic, retain) UIActivityIndicatorView *uiActivity;
 @property (nonatomic, retain) id commentDelegate;
+@property (nonatomic, retain) UIButton *uiRating1;
+@property (nonatomic, retain) UIButton *uiRating2;
+@property (nonatomic, retain) UIButton *uiRating3;
+@property (nonatomic, retain) UIButton *uiRating4;
+@property (nonatomic, retain) UIButton *uiRating5;
+@property (nonatomic, assign) NSUInteger rating;
 
 - (IBAction) uiSubmitted:(id)sender;
+- (IBAction) uiRatingPressed:(id)sender;
 
 @end
 
 @protocol CommentSubmitter
-- (void)submitComment:(NSString *)comment  activityIndicator:(UIActivityIndicatorView *)activity;
+- (void)submitComment:(NSString *)comment  activityIndicator:(UIActivityIndicatorView *)activity rating:(NSUInteger)rating;
 @end
--- joindinapp/Classes/NewCommentViewCell.m
+++ joindinapp/Classes/NewCommentViewCell.m
@@ -15,10 +15,59 @@
 @synthesize uiComment;
 @synthesize uiActivity;
 @synthesize commentDelegate;
+@synthesize uiRating1;
+@synthesize uiRating2;
+@synthesize uiRating3;
+@synthesize uiRating4;
+@synthesize uiRating5;
+@synthesize rating;
 
 - (IBAction) uiSubmitted:(id)sender {
 	self.uiSubmit.hidden = YES;
-	[self.commentDelegate submitComment:self.uiComment.text activityIndicator:self.uiActivity];
+	[self.commentDelegate submitComment:self.uiComment.text activityIndicator:self.uiActivity rating:self.rating];
+}
+
+- (IBAction) uiRatingPressed:(id)sender {
+	if (self.uiRating1.state == 1) {
+		self.rating = 1;
+		[self.uiRating1 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating2 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating3 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating4 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating5 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+	}
+	if (self.uiRating2.state == 1) {
+		self.rating = 2;
+		[self.uiRating1 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating2 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating3 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating4 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating5 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+	}
+	if (self.uiRating3.state == 1) {
+		self.rating = 3;
+		[self.uiRating1 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating2 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating3 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating4 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+		[self.uiRating5 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+	}
+	if (self.uiRating4.state == 1) {
+		self.rating = 4;
+		[self.uiRating1 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating2 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating3 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating4 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating5 setImage:[UIImage imageNamed:@"rating-off.gif"] forState:0];
+	}
+	if (self.uiRating5.state == 1) {
+		self.rating = 5;
+		[self.uiRating1 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating2 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating3 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating4 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+		[self.uiRating5 setImage:[UIImage imageNamed:@"rating-on.gif"]  forState:0];
+	}
 }
 
 @end
