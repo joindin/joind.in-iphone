7587210b5e8b908cc4b5418bcb282509e47448df
Kevin Bowman <kevin@magicmonkey.org>
2010-01-20 23:30:45 +0000

    Made the settings screen a bit nicer and allowed anonymous API usage

--- joindinapp/Classes/SettingsViewController.h
+++ joindinapp/Classes/SettingsViewController.h
@@ -14,16 +14,31 @@
 	IBOutlet UITextField  *uiPass;
 	IBOutlet UISwitch     *uiLimitEvents;
 	IBOutlet UIPickerView *uiAPIUrl;
+	IBOutlet UISwitch     *uiSignIn;
+	IBOutlet UIButton     *uiOk;
+	IBOutlet UILabel      *uiUserLabel;
+	IBOutlet UILabel      *uiPassLabel;
+	IBOutlet UILabel      *uiLimitLabel;
 }
 
 @property (nonatomic, retain) UITextField  *uiUser;
 @property (nonatomic, retain) UITextField  *uiPass;
 @property (nonatomic, retain) UISwitch     *uiLimitEvents;
 @property (nonatomic, retain) UIPickerView *uiAPIUrl;
+@property (nonatomic, retain) UISwitch     *uiSignIn;
+@property (nonatomic, retain) UIButton     *uiOk;
+@property (nonatomic, retain) UILabel      *uiUserLabel;
+@property (nonatomic, retain) UILabel      *uiPassLabel;
+@property (nonatomic, retain) UILabel      *uiLimitLabel;
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)thePickerView;
 - (NSInteger)pickerView:(UIPickerView *)thePickerView numberOfRowsInComponent:(NSInteger)component;
 - (NSString *)pickerView:(UIPickerView *)thePickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
 
+- (IBAction) changedSignIn:(UISwitch *)sender;
+- (IBAction) submitScreen:(id)sender;
+- (IBAction) doneEditing:(id)sender;
+- (void) setupSignedIn;
+- (void) savePrefs;
 
 @end
--- joindinapp/Classes/SettingsViewController.m
+++ joindinapp/Classes/SettingsViewController.m
@@ -8,6 +8,7 @@
 
 #import "SettingsViewController.h"
 #import "APICaller.h"
+#import "UserValidate.h"
 
 @implementation SettingsViewController
 
@@ -15,25 +16,25 @@
 @synthesize uiPass;
 @synthesize uiLimitEvents;
 @synthesize uiAPIUrl;
+@synthesize uiSignIn;
+@synthesize uiOk;
+@synthesize uiUserLabel;
+@synthesize uiPassLabel;
+@synthesize uiLimitLabel;
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 	NSUserDefaults *userPrefs = [NSUserDefaults standardUserDefaults];
-	self.uiUser.text = [userPrefs stringForKey:@"username"];
-	self.uiPass.text = [userPrefs stringForKey:@"password"];
+	self.uiUser.text      = [userPrefs stringForKey:@"username"];
+	self.uiPass.text      = [userPrefs stringForKey:@"password"];
 	self.uiLimitEvents.on = [userPrefs boolForKey:@"limitevents"];
+	self.uiSignIn.on      = [userPrefs boolForKey:@"uselogin"];
 	[self.uiAPIUrl selectRow:[userPrefs integerForKey:@"apiurl"] inComponent:0 animated:NO];
+	[self setupSignedIn];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
 	[super viewWillDisappear:animated];
-	NSUserDefaults *userPrefs = [NSUserDefaults standardUserDefaults];
-	[userPrefs setObject:self.uiUser.text forKey:@"username"];
-	[userPrefs setObject:self.uiPass.text forKey:@"password"];
-	[userPrefs setBool:self.uiLimitEvents.on forKey:@"limitevents"];
-	[userPrefs setInteger:[self.uiAPIUrl selectedRowInComponent:0] forKey:@"apiurl"];
-	[userPrefs synchronize];
-	[APICaller clearCache];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +43,72 @@
 
 - (void)dealloc {
     [super dealloc];
+}
+
+- (IBAction) changedSignIn:(UISwitch *)sender {
+	[self setupSignedIn];
+}
+
+- (void) setupSignedIn {
+	if (self.uiSignIn.on) {
+		self.uiUser.enabled        = YES;
+		self.uiPass.enabled        = YES;
+		self.uiLimitEvents.enabled = YES;
+		self.uiUser.hidden         = NO;
+		self.uiPass.hidden         = NO;
+		self.uiLimitEvents.hidden  = NO;
+		self.uiUserLabel.hidden    = NO;
+		self.uiPassLabel.hidden    = NO;
+		self.uiLimitLabel.hidden   = NO;
+	} else {
+		self.uiUser.enabled        = NO;
+		self.uiPass.enabled        = NO;
+		self.uiLimitEvents.enabled = NO;
+		self.uiUser.hidden         = YES;
+		self.uiPass.hidden         = YES;
+		self.uiLimitEvents.hidden  = YES;
+		self.uiUserLabel.hidden    = YES;
+		self.uiPassLabel.hidden    = YES;
+		self.uiLimitLabel.hidden   = YES;
+	}
+}
+
+- (IBAction) submitScreen:(id)sender {
+	if (self.uiSignIn.on) {
+		UserValidate *u = [APICaller UserValidate:self];
+		[u call:self.uiUser.text password:self.uiPass.text];		
+	} else {
+		[self savePrefs];
+		[self.navigationController popViewControllerAnimated:YES];
+	}
+}
+
+- (void) savePrefs {
+	NSUserDefaults *userPrefs = [NSUserDefaults standardUserDefaults];
+	[userPrefs setBool:self.uiSignIn.on      forKey:@"uselogin"];
+	[userPrefs setObject:self.uiUser.text    forKey:@"username"];
+	[userPrefs setObject:self.uiPass.text    forKey:@"password"];
+	[userPrefs setBool:self.uiLimitEvents.on forKey:@"limitevents"];
+	[userPrefs setInteger:[self.uiAPIUrl selectedRowInComponent:0] forKey:@"apiurl"];
+	[userPrefs synchronize];
+	[APICaller clearCache];
+}
+
+- (void)gotUserValidateData:(BOOL)success error:(APIError *)err {
+	if (success) {
+		[self savePrefs];
+		[self.navigationController popViewControllerAnimated:YES];
+	} else {
+		UIAlertView *alert;
+		alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Invalid username/password"
+										  delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		[alert show];
+		[alert release];
+	}
+}
+
+-(IBAction)doneEditing:(id)sender {
+	[sender resignFirstResponder];
 }
 
 #pragma mark UIPickerView methods
