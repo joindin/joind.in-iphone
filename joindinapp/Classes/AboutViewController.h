0cbf466f5f146ca58ab0c05c4bb928c6c122000d
Kevin Bowman <kevin@magicmonkey.org>
2010-02-01 21:47:26 +0000

    Made the rating images smaller

---
+++ joindinapp/Classes/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  joindinapp
+//
+//  Created by Kevin on 01/02/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutViewController : UIViewController {
+
+}
+
+@end
