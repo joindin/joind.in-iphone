d291613368c82969c94e8834eca4c3e1aac24b9f
Kevin <kevin@edoras.local>
2010-02-01 21:09:49 +0000

    Added talk rank display to event detail view, and noted some todos based on beta-testing feedback

---
+++ joindinapp/Classes/EventTalkViewCell.m
@@ -0,0 +1,19 @@
+//
+//  EventTalkViewCell.m
+//  joindinapp
+//
+//  Created by Kevin on 01/02/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "EventTalkViewCell.h"
+
+
+@implementation EventTalkViewCell
+
+@synthesize uiDetail;
+@synthesize uiTalkName;
+@synthesize uiSpeaker;
+@synthesize uiRating;
+
+@end
