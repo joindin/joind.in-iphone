bf315cecae497fcac6f41df4efb0ecd9bce02861
Kevin <kevin@edoras.local>
2010-02-07 10:48:49 +0000

    Started to learn how to do custom URLs

--- joindinapp/Classes/joindinappAppDelegate.m
+++ joindinapp/Classes/joindinappAppDelegate.m
@@ -31,6 +31,31 @@
 	// Save data if appropriate
 }
 
+// Handle someone opening a "joindin://talk/123" or "joindin://event/123" URL
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url 
+{
+	if (url == nil) {
+		return NO;
+	}
+	
+    NSString *URLString = [url absoluteString];
+	
+    //[[NSUserDefaults standardUserDefaults] setObject:URLString forKey:@"url"];
+    //[[NSUserDefaults standardUserDefaults] synchronize];
+	
+	/*
+	NSURL *logurl = [NSURL URLWithString:@"http://kevin.rivendell.local/?url2"];
+	NSError *error;
+	NSMutableURLRequest* liveRequest = [[NSMutableURLRequest alloc] initWithURL:logurl];
+	[liveRequest setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+	[liveRequest setValue:@"headervalue" forHTTPHeaderField:@"headerfield"];
+	NSURLResponse* response;
+	NSData* myData = [NSURLConnection sendSynchronousRequest:liveRequest returningResponse:&response error:&error];
+	NSString * checkResponse = [[NSString alloc] initWithData:myData encoding:NSASCIIStringEncoding];
+	*/
+	
+    return YES;
+}
 
 #pragma mark -
 #pragma mark Memory management
